e07b4f067e12f955126ee605d8f2445b73d17c97
Alina Goncharova <al.cherepnina@gmail.com>
2021-04-26 10:33:30 +0300

    Добавлены главная страница и страница с контактами

--- app/src/main/java/ru/goncharova/test1/MainActivity.java
+++ app/src/main/java/ru/goncharova/test1/MainActivity.java
@@ -2,14 +2,30 @@ package ru.goncharova.test1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonBack;
+        buttonBack = (Button)findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener((View.OnClickListener) this);
         int a = 10;
+        getSupportActionBar().hide();
     }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        i = new Intent(this, BackActivity.class);
+        startActivity(i);
+    }
+
+
 }
